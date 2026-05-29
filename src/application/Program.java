9017af9314b360a4cee492fbb4d0bd4d20ca5f52
Levi Paixão <levipaixao32@gmail.com>
2026-05-29 17:34:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entites.Department;
+import model.entites.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department("Books", 1);
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
