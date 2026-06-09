f8a2e8f10dedb77958dee5be02f57d67eed65ce0
Levi Paixão <levipaixao32@gmail.com>
2026-06-09 15:30:10 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,5 +38,12 @@ public class Program {
         Seller newSeller = new Seller(null,"Greg", "Greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted new Id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: Seller Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update Completed! ");
+
     }
 }
