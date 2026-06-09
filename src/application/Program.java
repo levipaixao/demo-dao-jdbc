66d14911431a271a20d4f1cc0a293f2cf744f13a
Levi Paixão <levipaixao32@gmail.com>
2026-06-09 15:39:33 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,12 @@ import model.entites.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -44,6 +47,13 @@ public class Program {
         seller.setName("Martha Wayne");
         sellerDao.update(seller);
         System.out.println("Update Completed! ");
+
+        System.out.println("\n=== TEST 6: Seller Delete ===");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deleted completed!");
+
 
     }
 }
